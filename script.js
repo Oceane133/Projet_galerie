90049ab579cf73c64bd3080391bc0e47a9ac80e7
Oceane133 <150061739+Oceane133@users.noreply.github.com>
2023-11-20 17:59:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+/* Afficher 1ere image */
+let slideIndex = 1;
+showSlides(slideIndex);
 
+/* Bouton suivant precedent */
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+/* Boucle */
+function showSlides(n) {
+  let i;
+const slides = document.getElementsByClassName("mySlides");
+if(n > slides.length) {slideIndex=1}
+if(n < 1) {slideIndex=slides.length}
+for (i=0; i<slides.length; i++) {
+	slides[i].style.display = "none";
+}
+slides[slideIndex-1].style.display = "block";
+}
