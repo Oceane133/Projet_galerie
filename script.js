7c5afbd4b2fb85d5078d84890a9691a0e3532730
cruzhamard <150705860+cruzhamard@users.noreply.github.com>
2023-11-29 19:50:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,19 +19,3 @@ for (i=0; i<slides.length; i++) {
 slides[slideIndex-1].style.display = "block";
 }
 
-/* Bouton voir plus */
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Voir plus"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Voir moins"; 
-    moreText.style.display = "inline";
-  }
-}
